bdb1cdedb30f5b9e0e607bd0729956fe164f13a8
Esma Goktekin Ornek <esma.goktekin@virasoft.com.tr>
2019-02-24 17:01:35 +0300

    Create the xcode project.

---
+++ OmMovies/OmMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OmMovies
+//
+//  Created by Esma on 2/24/19.
+//  Copyright © 2019 Esma Goktekin Ornek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
